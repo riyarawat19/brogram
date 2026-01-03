d943239151cf131eca5ce397be513168e5dd1da7
Riya Rawat <riyarawat.1904@gmail.com>
2026-01-03 20:58:26 +0530

    Initial commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,27 @@
+export default function Layout(props) {
+
+    const { children } = props
+
+    const header = (
+        <header>
+            <h1 className="text-gradient">The Brogram</h1>
+            <p><strong>The 30 Simple Workouts Program</strong></p>
+        </header>
+    )
+
+    const footer = (
+        <footer>
+            <p>Built by Riya Rawat <br />Styled with FantaCSS</p>
+        </footer>
+    )
+
+    //https://www,YOUR_USERNAME.netlify.app
+
+    return (
+        <>
+            {header}
+            {children}
+            {footer}
+        </>
+    )
+}
